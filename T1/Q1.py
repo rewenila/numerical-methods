e1c485cedae00ea92db14633b7b8cfb242fe950a
rewenila <32466162+rewenila@users.noreply.github.com>
2023-06-16 22:49:20 +0200

    Add files via upload

---
+++ T1/Q1.py
@@ -0,0 +1,10 @@
+
+eps = 1
+while ((eps+1)>1):
+        eps = eps/2
+
+eps = eps*2
+ 
+print(eps)
+
+
---
+++ T1/Q4.py
@@ -0,0 +1,19 @@
+
+n = 0
+pi_ant = 0
+
+for x in range(50):
+        pi = pi_ant + 4*(((-1)**n)/(2*n+1))
+        print(pi)
+        
+        pi_ant = pi
+        n = n+1
+
+                         
+                           
+        
+
+
+
+
+
